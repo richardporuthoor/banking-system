98f556f79271a8fa82064f16a82240a66771178c
Matthew <kutynamatthew@gmail.com>
2025-09-28 17:38:15 -0500

    created the main_menu()

---
+++ bankingsystem1.py
@@ -0,0 +1,25 @@
+def main_menu():
+    print("1. Create Account")
+    print ("2. Deposit")
+    print("3. Withdrawl")
+    print("4. Check Balance")
+    print("5 .Exit")
+
+    choice = input("Enter a selection: ")
+
+    if choice == "1":
+        print("selected option 1")
+    elif choice == "2":
+        print("Selected Options 2")
+    elif choice == "3":
+        print("Selected Option 3")
+    elif choice == "4":
+        print("Selected Option 4")
+    elif choice == "5":
+        print("Selected Option 5")
+    else:
+        print("Invalid Command")
+
+
+
+
